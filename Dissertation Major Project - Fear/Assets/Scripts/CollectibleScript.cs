c1d92fff95e4f0ac67d579923af7d19dfc72ad2a
PAL15365792 <PAL15365792@students.solihull.ac.uk>
2020-04-21 20:01:04 +0100

    Dissertation Horror Game Version 1
    Here is the first version of my dissertation project, a simple horror game where the player must find and collect 8 skulls around the various landmarks of the forest. There are no enemies, however there are several jumpscares in various locations. This may be worked on after I have finished University.

---
+++ Dissertation Major Project - Fear/Assets/Scripts/CollectibleScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            other.GetComponent<PlayerScript>().points++; // Adds 1 to points.
+            Destroy(gameObject); // This destroys the collectible.
+        }
+    }
+}
